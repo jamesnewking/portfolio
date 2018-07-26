98c1841877e72e8678bcd488551219e04d14bbbc
James Wang <jamesnewking@gmail.com>
2018-07-25 22:31:04 -0700

    merged mailer code

---
+++ php_mailer/email_config.conf.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','<YOUR EMAIL ADDRESS>');
+define('EMAIL_USERNAME','<YOUR DISPLAY NAME>');
+define('EMAIL_PASS','<YOUR PASSWORD>');
+define('EMAIL_TO_ADDRESS','<EMAIL to send to>');
+?>
---
+++ php_mailer/email_config.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','jwwwportfolio@gmail.com');
+define('EMAIL_USERNAME','James');
+define('EMAIL_PASS','KimoKobe');
+define('EMAIL_TO_ADDRESS','jamesnewking@gmail.com');
+?>
