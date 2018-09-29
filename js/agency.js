869d63249b45fe178bf969916bd28bd4482fbb5e
James Wang <jamesnewking@gmail.com>
2018-09-29 14:46:43 -0700

    added typewriter affect

--- js/agency.js
+++ js/agency.js
@@ -70,6 +70,67 @@
   })
   $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
-  })
+  });
+
+//typewriter code
+    class TypeWriter {
+        constructor(txtElement, words, wait = 2000) {
+            this.txtElement = txtElement;
+            this.words = words;
+            this.txt = '';
+            this.wordIndex = 0;
+            this.wait = parseInt(wait, 10);
+            this.type();
+            this.isDeleting = false;
+        }
+
+        type() {
+            // Current index of word
+            const current = this.wordIndex % this.words.length;
+            // Get full text of current word
+            const fullTxt = this.words[current];
+
+            // Check if deleting
+            if(this.isDeleting) {
+                // Remove char
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                // Add char
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+
+            // Insert txt into element
+            this.txtElement.textContent = `${this.txt}`;
+
+            // Initial Type Speed
+            let typeSpeed = 200;
+
+            if(this.isDeleting) {
+                typeSpeed /= 2;
+            }
+
+            // If word is complete
+            if(!this.isDeleting && this.txt === fullTxt) {
+                // Make pause at end
+                typeSpeed = this.wait;
+                // Set delete to true
+                this.isDeleting = true;
+            } else if(this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                // Move to next word
+                this.wordIndex++;
+                // Pause before start typing
+                typeSpeed = 200;
+            }
+
+            setTimeout(() => this.type(), typeSpeed);
+        }
+    }
+
+    const txtElement = document.querySelector('#typewriter');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
 
 })(jQuery); // End of use strict
